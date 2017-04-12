315bc25f66708eac045633c761be78933b31e02d
Happypig375 <hadrianwttang@outlook.com>
2017-04-12 22:31:29 +0800

    The very very basic commands - end, print

---
+++ MCRealm/MCRealm/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MCRealm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Jar = "";
+            while (true)
+            {
+                var Command = Console.ReadLine();
+                var Content = Command.Substring(Command.IndexOf(' ') + 1);
+                switch (Command.Remove(Command.IndexOf(' ')).ToLowerInvariant())
+                {
+                    case "jar":
+                        Jar = Content;
+                        break;
+                    case "start":
+                        break;
+                    case var s when s.StartsWith("/"):
+                        break;
+                    default:
+                        break;
+                    case "print":
+                        Console.WriteLine(Content);
+                        break;
+                    case "end":
+                        goto Exit;
+                }
+            }
+            Exit:;
+        }
+    }
+}
